e11302aa03638de102bb8f412e531a3d6e8f37a7
肖宇含 <yuhanx@andrew.cmu.edu>
2019-01-21 01:47:27 -0500

    resolve API issue

--- test_req.py
+++ test_req.py
@@ -1,5 +1,5 @@
 import requests
-apiKey = "AIzaSyBBGvr3EgXKk4tXm4d6Hrji5X3j4TiSgl0"
+apiKey = "" # hidden
 url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Murray%20Avenue%20at%20Hobart%20Street&inputtype=textquery&key="+apiKey
 r = requests.get(url)
 placeID = (r.json()['candidates'][0]['place_id'])
